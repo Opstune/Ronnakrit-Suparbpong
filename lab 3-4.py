04e873bfdcd9a58c07d8a31f1970162a9e7ff5c5
Opstune <mawinrnk@gmail.com>
2024-06-19 12:21:54 +0700

    update

---
+++ lab 3-4.py
@@ -0,0 +1,48 @@
+num_students = int(input("นักเรียนกี่คน: "))
+total = 0 
+
+for i in range(num_students):
+    score = int(input(f"กรุณาป้อนคะแนนนักเรียนคนที่ {i+1}: "))
+    total += score
+
+print(f"ผลรวมคะแนนทั้งหมดคือ: {total}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#num = int(input("รับค่าเลข : "))
+#while num >= 0:
+#    print(num**2)
+#    num = int(input("รับค่าคะแนน : "))
+    
+#print("congrates u got ",num)
+
+
+#git status
+#git add .
+#git commit -m "update"
+#git push
+
+#x = int(input("รับค่าแม่สูตรคูณ :"))
+#y = 1
+#while y <= 12:
+ #   z = x * y
+  #  print("%d x %d = %d" % (x, y, z))
+  #  y+=1
+  
+  
